194e92f5b349bc4cb60ec4585d1492af02657ffe
JCamiloRamirezTabares <juancaram21@gmail.com>
2023-12-03 18:32:48 -0500

    Push commit

--- Client/src/main/java/ICEClientHandler.java
+++ Client/src/main/java/ICEClientHandler.java
@@ -1,12 +1,50 @@
+import com.zeroc.Ice.Communicator;
+import Ice.ObjectPrx;
+import Ice.Util;
+import SortingSystem.SortingMasterPrx;
+import SortingSystem.SortingMasterPrxHelper;
+
 public class ICEClientHandler {
+    private Communicator iceCommunicator;
     private SortingMasterPrx sortingMasterProxy;
 
     public ICEClientHandler() {
-        // Iniciar ICE y crear el proxy ()
+        // Inicializa el ICE communicator y crea el proxy
+        try {
+            iceCommunicator = Util.initialize();
+            String proxyString = "SortingMaster:default -p 10000"; // Ejemplo de proxyString
+            ObjectPrx base = iceCommunicator.stringToProxy(proxyString);
+            sortingMasterProxy = SortingMasterPrxHelper.checkedCast(base);
+            if (sortingMasterProxy == null) {
+                throw new Error("Invalid proxy");
+            }
+        } catch (Ice.LocalException e) {
+            e.printStackTrace();
+        }
     }
 
     public void sendSortingRequest(String filePath) {
-        // Envia la sorting request al SortingMaster
+        // Enviar la solicitud de ordenamiento al SortingMaster
+        try {
+            sortingMasterProxy.sortData(filePath);
+        } catch (Ice.LocalException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String receiveSortedData() {
+        // Implementar lógica para recibir los datos ordenados
+        // Esto puede depender de cómo está configurada tu aplicación
+        return "sorted data"; // Esto es un placeholder
+    }
+
+    public void closeConnection() {
+        if (iceCommunicator != null) {
+            try {
+                iceCommunicator.destroy();
+            } catch (Ice.LocalException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
-
--- Client/src/main/java/SortingClient.java
+++ Client/src/main/java/SortingClient.java
@@ -1,5 +1,3 @@
-
-
 public class SortingClient {
     private ICEClientHandler iceHandler;
     private LatencyTracker latencyTracker;
@@ -8,14 +6,41 @@ public class SortingClient {
     public SortingClient() {
         this.iceHandler = new ICEClientHandler();
         this.latencyTracker = new LatencyTracker();
-        this.dataReceiver = new DataReceiver();
+        this.dataReceiver = new DataReceiver(iceHandler);
     }
 
     public void requestFileSorting(String filePath) {
-        latencyTracker.start();
-        iceHandler.sendSortingRequest(filePath);
-        List<String> sortedData = dataReceiver.receiveSortedData();
-        latencyTracker.stop();
+        try {
+            latencyTracker.start();
+            iceHandler.sendSortingRequest(filePath); // Envía la solicitud de ordenamiento
+            String sortedData = dataReceiver.receiveSortedData(); // Recibe los datos ordenados
+            handleSortResponse(sortedData);
+        } catch (Exception e) {
+            ErrorHandling.handleError(e);
+        } finally {
+            calculateAndDisplayLatency();
+        }
+    }
+
+    private void handleSortResponse(String sortedData) {
+        if (sortedData != null && !sortedData.isEmpty()) {
+            displaySortedData(sortedData);
+        } else {
+            System.out.println("No data received or data is empty.");
+        }
+    }
+
+    private void displaySortedData(String sortedData) {
+        // Aquí puedes implementar una lógica más compleja para mostrar los datos
         System.out.println("Sorted Data: " + sortedData);
+    }
+
+    public void calculateAndDisplayLatency() {
+        latencyTracker.stop();
+        System.out.println("Latency: " + latencyTracker.getLatency() + " ms");
+    }
+
+    public void shutdown() {
+        iceHandler.closeConnection(); // Asegúrate de que ICEClientHandler tenga este método
     }
 }
